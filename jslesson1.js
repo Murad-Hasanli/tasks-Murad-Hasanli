bcea70de3dedc24384f3afb42dfd3201d0086fd8
Murad kh. Hasanli <muradkhh@code.local>
2021-10-20 18:19:38 +0400

    added tasks

---
+++ jslesson1.js
@@ -0,0 +1,18 @@
+
+/* write a javascript function that reverse a number
+ */
+/* function reverse_a_number(n)
+{
+	n = n + "";
+	return n.split("").reverse().join("");
+}
+console.log(reverse_a_number(32243)); */
+/* 
+
+
+/* alphabetical order. */
+/* function alphabetOrder(str)
+  {
+return str.split('').sort().join('');
+  }
+console.log(alphabetOrder("webmaster")); */ 
